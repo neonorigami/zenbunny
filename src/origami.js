e57368a8eca36c8f3fc56033048ade347e16656f
Richard <richard.anaya@gmail.com>
2014-03-14 03:08:43 -0500

    new pointer method

--- src/origami.js
+++ src/origami.js
@@ -1,3 +1,111 @@
+(function() {
+    CocoonJS.Audio= function() {
+        return this;
+    };
+
+    CocoonJS.Audio.prototype= {
+
+        audio   : null,
+
+        setAudio : function( audio ) {
+            this.audio= audio;
+            this.audio.load();
+            return this;
+        },
+
+        loop : function( loop ) {
+            return this;
+        },
+
+        play : function() {
+            //var volume = Math.random();
+            //console.log("volume = " + volume);
+            this.audio.volume = 0.5;
+
+            this.audio.play();
+            return this;
+        },
+
+        pause : function() {
+            this.audio.pause();
+            return this;
+        }
+
+    };
+
+})();
+
+(function() {
+    CocoonJS.Music= function() {
+        return this;
+    };
+
+    CocoonJS.Music.prototype= {
+
+        audio   : null,
+
+        setAudio : function( audio ) {
+            this.audio= audio;
+            this.audio.load();
+            this.audio.addEventListener(
+                'ended',
+                function(audioEvent) {
+                    audioEvent.target.playing= false;
+                    console.log("Audio ends playing.");
+                },
+                false
+            );
+            return this;
+        },
+
+        loop : function( ) {
+            if ( !this.audio) {
+                console.log("audio not present.");
+                return;
+            }
+
+            this.audio.loop= !this.audio.loop;
+            return this;
+        },
+
+        play : function() {
+
+            if ( !this.audio) {
+                console.log("audio not present.");
+                return;
+            }
+
+            if ( this.audio.playing ) {
+                return;
+            }
+            this.audio.playing= true;
+
+            //var volume = Math.random();
+            //console.log("volume = " + volume);
+            //this.audio.volume = volume;
+
+            this.audio.play();
+
+
+            return this;
+        },
+
+        pause : function() {
+            if ( !this.audio) {
+                console.log("audio not present.");
+                return;
+            }
+            this.audio.pause();
+            this.audio.playing= false;
+            return this;
+        }
+
+    };
+
+})();
+
+
+
 // create an new instance of a pixi stage
 var stage = new PIXI.Stage(0x66FF99);
 
@@ -44,9 +152,19 @@ var pointerDown = function(x,y){
 
     animals.push(bunny);
     console.log("play sound")
-    new Howl({
-        urls: ['gong.ogg','gong.mp3']
-    }).play();
+
+    if(navigator.isCocoonJS){
+        var s01= new Audio();
+        s01.src= "gong.ogg";
+        var c = new CocoonJS.Audio();
+        c.setAudio(s01);
+        c.play();
+    }
+    else {
+        new Howl({
+            urls: ['gong.ogg','gong.mp3']
+        }).play();
+    }
 };
 
 if(navigator.isCocoonJS){
